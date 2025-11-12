1435ee545309752f00cc8d2d9c1a0ade9debdd44
AydanBayramova <bayramoffaaydan1@gmail.com>
2025-11-12 13:39:41 +0400

    FindTheDifference question in Leetcode solved

---
+++ src/strings/FindTheDifference.java
@@ -0,0 +1,19 @@
+package strings;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class FindTheDifference {
+    public char findTheDifference(String s, String t) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (char c : s.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+
+        }
+        for (char c : t.toCharArray()) {
+            if (!map.containsKey(c) || map.get(c) == 0)  return c;
+            else map.put(c, map.getOrDefault(c, 0) - 1);
+        }
+        return ' ';
+    }
+}
