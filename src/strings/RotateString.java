dbbe8903bb4989bf23e65df177fa5c8a80c8ff92
AydanBayramova <bayramoffaaydan1@gmail.com>
2026-03-23 14:21:10 +0400

    796-RotateString leetcode's question has been written

---
+++ src/strings/RotateString.java
@@ -0,0 +1,11 @@
+package strings;
+
+public class RotateString {
+    public boolean rotateString(String s, String goal) {
+        if(s.length()!=goal.length()){
+            return false;
+        }
+        String word=s+s;
+        return word.contains(goal);
+    }
+}
