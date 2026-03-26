d5ebe1585959da92edd8dc006b2a487971b930bd
AydanBayramova <bayramoffaaydan1@gmail.com>
2026-03-26 22:33:04 +0400

    Contains Duplicate question has benn written

---
+++ src/HashSet/ContainsDuplicate.java
@@ -0,0 +1,15 @@
+package HashSet;
+
+import java.util.HashSet;
+
+public class ContainsDuplicate {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> hashSet=new HashSet<>();
+        for(int i=0; i<nums.length;i++){
+            if(!hashSet.add(nums[i])){
+                return true;
+            }
+        }
+        return false;
+    }
+}
