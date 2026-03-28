b3bc7444a61aa1f01b9b4cafd4038225eeef0802
AydanBayramova <bayramoffaaydan1@gmail.com>
2026-03-28 13:21:27 +0400

    Fibonacci Number question has been written using Dynamic Programming

---
+++ src/recursion/FibonacciNumber.java
@@ -0,0 +1,12 @@
+package recursion;
+
+public class FibonacciNumber {
+    int[] memo=new int[31];
+    public int fib(int n) {
+        if(n==0) return 0;
+        if(n==1) return 1;
+        if(memo[n]!=0) return memo[n];
+        memo[n]=fib(n-1)+fib(n-2);
+        return memo[n];
+    }
+}
